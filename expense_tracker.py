076763b1604a2cfb93da66a2da7b5646cc31ed9b
Beatrice1000 <129980791+Beatrice1000@users.noreply.github.com>
2024-06-06 21:26:33 +0200

    Add files via upload

--- expense_tracker.py
+++ expense_tracker.py
@@ -3,27 +3,26 @@ import calendar
 import datetime
 
 def main():
-    print (f"Starting Expense Tracker!")
-    expense_file_name ="expenses.csv"
+    print(f"Starting Expense Tracker!")
+    expense_file_path = "expenses.csv"
     budget = 2000
 
     # Get user input for expenses.
     expense = get_user_expense()
-    print(expense)
+    # print(expense)
 
     # Write user expense to a file.
-    save_expense_to_file(expense, expense_file_name)
+    save_expense_to_file(expense, expense_file_path)
 
     # Read file and summarize expenses.
-    summarize_expenses(expense_file_name, budget)
-
+    summarize_expenses(expense_file_path, budget)
 
 
 def get_user_expense():
     print(f"Getting User Expense")
-    expense_name = input("Enter expense name:")
-    expense_amount = input("Enter expense amount:")
-    #print(f"You have entered {expense_name}, {expense_amount}")
+    expense_name = input("Enter expense name: ")
+    expense_amount = float(input("Enter expense amount: "))
+    # print(f"You have entered {expense_name}, {expense_amount}")
 
     expense_categories = [
         "Food", "Rent", "Clothing", "Transport", "Insurance", "Fun", "Miscellaneous"]
@@ -39,34 +38,33 @@ def get_user_expense():
         if selected_index in range(len(expense_categories)):
             selected_category = expense_categories[selected_index]
             new_expense = Expense(
-                name=expense_name, category = selected_category, amount = expense_amount)
+                name=expense_name, category=selected_category, amount=expense_amount)
             return new_expense
         else:
             print("Invalid category. Please try again")
 
 
-
-
-def save_expense_to_file(expense, expense_file_name):
-    print(f"Save User Expense: {expense} to {expense_file_name}")
-    with open(expense_file_name, "a") as f:
+def save_expense_to_file(expense: Expense, expense_file_path):
+    print(f"Save User Expense: {expense} to {expense_file_path}")
+    with open(expense_file_path, "a") as f:
         f.write(f"{expense.name}, {expense.category}, {expense.amount}\n")
-    pass
 
-def summarize_expenses(expense_file_path, budget):
-    print(f" Summarizing User Expense")
+
+
+def summarize_expenses(expense_file_path,budget):
+    print(f"Summarizing User Expense")
     expenses: list[Expense] = []
     with open(expense_file_path, "r") as f:
         lines = f.readlines()
         for line in lines:
-            print(line)
-            expense_name, expense_amount, expense_category = line.strip().split(",")
+            expense_name, expense_category, expense_amount = line.strip().split(",")
             line_expense = Expense(
                 name=expense_name,
                 amount=float(expense_amount),
                 category=expense_category,
             )
             expenses.append(line_expense)
+
 
     amount_by_category = {}
     for expense in expenses:
@@ -78,25 +76,24 @@ def summarize_expenses(expense_file_path, budget):
 
     print("Expenses By Category 📈:")
     for key, amount in amount_by_category.items():
-        print(f"  {key}: ${amount:.2f}")
+        print(f"  {key}: €{amount:.2f}")
 
-    total_spent = sum([x.amount for x in expenses])
-    print(f"💵 Total Spent: ${total_spent:.2f}")
+    total_spent = sum([expense.amount for expense in expenses])
+    print(f"💵 Total Spent: €{total_spent:.2f}")
 
     remaining_budget = budget - total_spent
-    print(f"✅ Budget Remaining: ${remaining_budget:.2f}")
+    print(f"✅ Budget Remaining: €{remaining_budget:.2f}")
 
     now = datetime.datetime.now()
     days_in_month = calendar.monthrange(now.year, now.month)[1]
     remaining_days = days_in_month - now.day
 
     daily_budget = remaining_budget / remaining_days
-    print(green(f"👉 Budget Per Day: ${daily_budget:.2f}"))
+    print(green(f"👉 Budget Per Day: €{daily_budget:.2f}"))
 
 
 def green(text):
     return f"\033[92m{text}\033[0m"
-
 
 
 if __name__ == "__main__":
